e5d10cf50a367753ebb056ff7abbab4f034dde78
Yakup-TBR <yakupasmaidyatanggae@gmail.com>
2024-04-21 02:03:36 +0800

    Mengubah dan memecah metode getAnnualIncomeTax()
    Pada metode getAnnualIncomeTax(), logika untuk menghitung berapa bulan pegawai bekerja dalam setahun dipindahkan ke metode yang ditambahkan yaitu calculateMonthsWorkedInYear()

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -90,16 +90,16 @@ public class Employee {
 	}
 	
 	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
+		int monthsWorkedInYear = calculateMonthsWorkedInYear();
+		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthsWorkedInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
+	}
+	
+	private int calculateMonthsWorkedInYear() {
+		LocalDate currentDate = LocalDate.now();
+		if (currentDate.getYear() == yearJoined) {
+			return currentDate.getMonthValue() - monthJoined;
+		} else {
+			return 12;
 		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
 	}
 }
